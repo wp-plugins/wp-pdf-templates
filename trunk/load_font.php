efa4a43d325eb48f0496ae386eb273706b22d352
Zuige <Zuige@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-03 13:23:03 +0000

    PHP 5.2 compatibility added, upgraded to newest version of dompdf lib
    git-svn-id: https://plugins.svn.wordpress.org/wp-pdf-templates/tags/1.3@959533 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/load_font.php
@@ -0,0 +1,22 @@
+#!/usr/bin/php
+<?php
+/**
+ * Filename: load_font.php
+ * Project: Wordpress PDF Templates
+ * Copyright: (c) 2014 Seravo
+ * License: GPLv3
+ *
+ * Use this to load a font from the command line.
+*/
+
+/*
+ * Require the WP Environment
+ */
+
+// This is where we require wp-load. Just uncomment the line below.
+require_once "../../../wp-load.php";
+
+/*
+ * Pass to the DOMPDF load_font command line tool
+ */
+require_once "dompdf/load_font.php";
